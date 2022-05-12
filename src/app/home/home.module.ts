30c8e5787986206770b0999a8351ce5470c4cedb
Maria Alice <maria_alice_172@hotmail.com>
2022-05-12 20:15:09 -0300

    add calendar and modal

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
-
+import { NgCalendarModule } from 'ionic2-calendar';
+import { CalModelPageModule } from '../pages/cal-model/cal-model.module';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt, 'pt-BR');
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgCalendarModule,
+    CalModelPageModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,156 @@
-import { Component } from '@angular/core';
-
+/* eslint-disable no-var */
+import { formatDate } from '@angular/common';
+import { Component, Inject, LOCALE_ID, OnInit, ViewChild } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
+import { CalendarComponent } from 'ionic2-calendar';
+import { CalModelPage } from '../pages/cal-model/cal-model.page';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  eventSource = [];
+  viewTitle: string;
 
-  constructor() {}
+  calendar = {
+    mode: 'month',
+    currentDate: new Date(),
+  };
 
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @ViewChild(CalendarComponent) myCall: CalendarComponent;
+  myCal: any;
+
+  constructor(
+    private alertCtrl: AlertController,
+    @Inject(LOCALE_ID) private locale: string,
+    private modalCtrl: ModalController
+  ) {}
+
+  ngOnInit() { }
+
+  next() {
+    this.myCall.slideNext();
+  }
+
+  back() {
+    this.myCall.slidePrev();
+  }
+
+  onViewTitleChanged(title: string) {
+    this.viewTitle = title;
+  }
+  async onEventSelected(event) {
+    // Use Angular date pipe for conversion
+    const start = formatDate(event.startTime, 'medium', this.locale);
+    const end = formatDate(event.endTime, 'medium', this.locale);
+
+    const alert = await this.alertCtrl.create({
+      header: event.title,
+      subHeader: event.desc,
+      message: 'From: ' + start + '<br><br>To: ' + end,
+      buttons: ['OK'],
+    });
+    alert.present();
+  }
+
+  createRandomEvents() {
+    const events = [];
+    for (let i = 0; i < 50; i += 1) {
+      const date = new Date();
+      const eventType = Math.floor(Math.random() * 2);
+      const startDay = Math.floor(Math.random() * 90) - 45;
+      let endDay = Math.floor(Math.random() * 2) + startDay;
+      var startTime;
+      var endTime;
+      if (eventType === 0) {
+        startTime = new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate() + startDay
+          )
+        );
+        if (endDay === startDay) {
+          endDay += 1;
+        }
+        endTime = new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate() + endDay
+          )
+        );
+        events.push({
+          title: 'All Day - ' + i,
+          startTime,
+          endTime,
+          allDay: true,
+        });
+      } else {
+        const startMinute = Math.floor(Math.random() * 24 * 60);
+        const endMinute = Math.floor(Math.random() * 180) + startMinute;
+        startTime = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + startDay,
+          0,
+          date.getMinutes() + startMinute
+        );
+        endTime = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + endDay,
+          0,
+          date.getMinutes() + endMinute
+        );
+        events.push({
+          title: 'Event - ' + i,
+          startTime,
+          endTime,
+          allDay: false,
+        });
+      }
+    }
+    this.eventSource = events;
+  }
+  removeEvents() {
+    this.eventSource = [];
+  }
+
+  async openCalModel() {
+    const modal = await this.modalCtrl.create({
+      component: CalModelPage,
+      cssClass: 'cal-modal',
+      backdropDismiss: false
+    });
+
+    await modal.present();
+
+    modal.onDidDismiss().then((result) => {
+      if (result.data && result.data.event) {
+        const event = result.data.event;
+        if (event.allDay) {
+          const start = event.startTime;
+          event.startTime = new Date(
+            Date.UTC(
+              start.getUTCFullYear(),
+              start.getUTCMonth(),
+              start.getUTCDate()
+            )
+          );
+          event.endTime = new Date(
+            Date.UTC(
+              start.getUTCFullYear(),
+              start.getUTCMonth(),
+              start.getUTCDate() + 1
+            )
+          );
+        }
+        this.eventSource.push(result.data.event);
+        this.myCal.loadEvents();
+      }
+    });
+  }
 }
